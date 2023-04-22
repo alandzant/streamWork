b43129bbbb7d7a78bafc48ef64aacf564078d95d
alandzant <127549234+alandzant@users.noreply.github.com>
2023-04-22 18:53:00 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.util.stream.Collectors;
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+
+        //ищем количество несовершеннолетних
+        long underage = persons.stream().filter(person -> person.getAge()<18).count();
+        System.out.println(underage);
+
+        //фамилии призывников
+        List<String> surnameConscript = persons.stream().filter(person -> person.getSex() == Sex.MAN).filter(person -> person.getAge() >=18)
+                .filter(person -> person.getAge() <27).map(person -> person.getFamily()).collect(Collectors.toList());
+
+
+        //фамилии работоспособных людей с высшим образованием
+        List<Person> higherEducationPeople = persons.stream().filter(person -> person.getEducation() == Education.HIGHER).filter(person -> person.getAge() >=18)
+                .filter(person -> (person.getSex() == Sex.MAN && person.getAge() <65) || (person.getSex() == Sex.WOMAN && person.getAge() <60))
+                .sorted(Comparator.comparing(Person::getFamily)).collect(Collectors.toList());
+
+    }
+}
